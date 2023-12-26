64f3e72a8a9826b513dbf6e40a01dd01f3e441cc
naren4567 <narendraitpro@gmail.com>
2023-12-26 18:07:39 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,11 +2,15 @@
 
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0.$TIMESTAMP.log"
 echo "script name: $0
  
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+
+if [ $1 -ne 0 ]
 then 
    echo "ERROR:: $2 INSTALLING MYSQL IS FAILED"
    exit 1 
@@ -22,10 +26,12 @@ then
 else
     echo "your root user"
 fi 
-yum install mysql -y
+
+
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? "mysql installation"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? " git installation"
