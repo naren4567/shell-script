952c298d71c67023bad4f4cc44a7092af7e90355
naren4567 <narendraitpro@gmail.com>
2023-12-26 17:41:18 +0530

    added 2nd script

---
+++ 11-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+ID=$(id -u)
+echo "script name: $0
+ 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then 
+   echo "ERROR:: $2 INSTALLING MYSQL IS FAILED"
+   exit 1 
+else
+    echo "$2 INSTALLING MYSQL IS success"
+fi
+
+}
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
+    exit 1
+else
+    echo "your root user"
+fi 
+yum install mysql -y
+
+VALIDATE $? "mysql installation"
+
+yum install git -y
+
+VALIDATE $? " git installation"
