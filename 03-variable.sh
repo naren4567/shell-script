a63fd4623898df147460081b66602b4916550a59
naren4567 <narendraitpro@gmail.com>
2023-12-26 15:33:37 +0530

    added 2nd script

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,8 +1,6 @@
 #!/bib/bash
-PERSON1=Ramesh
-PERSON12=Suresh
 
-echo "$PERSON1 :: Hello $PERSON2 good mornung"
-echo "$PERSON2 :: Hello $PERSON1 very good mornung"
-echo "$PERSON1 :: how are you $PERSON2"
-echo "$PERSON1 :: iam good"
+DATE=$(date)
+
+echo "date and time is :${DATE}"
+
