a5b34392a5867464edcffd6d8bbc102776c9d1ac
naren4567 <narendraitpro@gmail.com>
2023-12-26 15:40:58 +0530

    added 2nd script

--- 04-variable.sh
+++ 04-variable.sh
@@ -6,4 +6,4 @@ PERSON12=$2
 echo "$PERSON1 :: Hello $PERSON2 good mornung"
 echo "$PERSON2 :: Hello $PERSON1 very good mornung"
 echo "$PERSON1 :: how are you $PERSON2"
-echo "$PERSON1 :: iam good"
+echo "$PERSON1 :: iam good $PERSON2"
