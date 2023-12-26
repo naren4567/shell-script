1fbe4735c81a6628a698fce1d1adf4cb25d11af3
naren4567 <narendraitpro@gmail.com>
2023-12-26 15:58:38 +0530

    added 2nd script

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,6 +1,8 @@
 #!/bib/bash
 
-USERNAME=$1
-PASSWORD=$2
+echo "please enter your username and password:"
+
+read -s USERNAME #THE VALUE ENTERD ABOVE WILL BE AUTOMATICALLY ATTACHED TO VARIABLE
+read -s PASSWORD 
 
 echo "Username" is: $USERNAME, "Password" is: $PASSWORD
