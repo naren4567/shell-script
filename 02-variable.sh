83c9735da8f4e52ffd4299471ac325f5c48fae98
naren4567 <narendraitpro@gmail.com>
2023-12-26 15:46:22 +0530

    added 2nd script

--- 02-variable.sh
+++ 02-variable.sh
@@ -2,7 +2,7 @@
 PERSON1=Robert
 PERSON12=John
 
-echo "$PERSON1 :: Hello $PERSON2 good mornung"
-echo "$PERSON2 :: Hello $PERSON1 very good mornung"
-echo "$PERSON1 :: how are you $PERSON2"
-echo "$PERSON2 :: iam good $PERSON1"
+echo "$PERSON1 : Hello $PERSON2 good mornung"
+echo "$PERSON2 : Hello $PERSON1 very good mornung"
+echo "$PERSON1 : how are you $PERSON2"
+echo "$PERSON2 : iam good $PERSON1"
--- 04-variable.sh
+++ 04-variable.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON12=$2
 
-echo "$PERSON1 :: Hello $PERSON2 good mornung"
-echo "$PERSON2 :: Hello $PERSON1 very good mornung"
-echo "$PERSON1 :: how are you $PERSON2"
-echo "$PERSON2 :: iam good $PERSON1"
+echo "$PERSON1 : Hello $PERSON2 good mornung"
+echo "$PERSON2 : Hello $PERSON1 very good mornung"
+echo "$PERSON1 : how are you $PERSON2"
+echo "$PERSON2 : iam good $PERSON1"
